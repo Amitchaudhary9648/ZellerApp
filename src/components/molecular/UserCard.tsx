b40fb5ed4de3e39020263165c52dd06dcdd88211
Amit Chaudhary <amit@ith.tech>
2024-09-20 07:49:48 +0530

    add the search feature in the userCard

--- src/components/molecular/UserCard.tsx
+++ src/components/molecular/UserCard.tsx
@@ -7,24 +7,27 @@ interface UserCardProps {
     name: string;
     role: string;
     activeUser: string;
+    userNameFilter: string;
 }
 
 const UserCard: FC<UserCardProps> = ({
     name,
     role,
-    activeUser
+    activeUser,
+    userNameFilter
 }) => {
+    const matchesFilter = name?.toLowerCase().includes(userNameFilter.toLowerCase());
     return (
         <View>
             {
-                activeUser.toLocaleUpperCase() == role.toLocaleUpperCase() ? (
+                activeUser.toLocaleUpperCase() == role.toLocaleUpperCase() && matchesFilter ? (
                     <View style={styles.container}>
                         <View style={styles.namePlaceholder}>
-                            <CustomText variant='h3' style={styles.placeHolderText}>{name?.slice(0, 1)}</CustomText>
+                            <CustomText variant='h5' style={styles.placeHolderText}>{name?.slice(0, 1)}</CustomText>
                         </View>
                         <View style={styles.textContainer}>
                             <CustomText variant='h5' style={styles.nameText}>{name}</CustomText>
-                            <CustomText variant='h6'>{role}</CustomText>
+                            <CustomText variant='h6'>{role?.slice(0,1)}{role?.slice(1,).toLowerCase()}</CustomText>
                         </View>
                     </View>
                 ) : (null)
@@ -36,13 +39,14 @@ const UserCard: FC<UserCardProps> = ({
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        marginBottom: 10
+        marginBottom: 25,
+        alignItems: 'center',
     },
     namePlaceholder: {
         backgroundColor: '#E8F2Fb',
-        width: 60,
-        height: 60,
-        borderRadius: 10,
+        width: 45,
+        height: 45,
+        borderRadius: 6,
         justifyContent: 'center',
         alignItems: 'center'
     },
