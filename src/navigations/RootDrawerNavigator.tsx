c87cddb192cb480c465ab1a1b49b9405d868fbe7
Amit Chaudhary <amit@ith.tech>
2024-09-20 07:47:57 +0530

    add the icons on drawer navigator

--- src/navigations/RootDrawerNavigator.tsx
+++ src/navigations/RootDrawerNavigator.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import UserScreen from '../screens/UserScreen/UserScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import { NAVIGATION_TO_HOME_SCREEN, NAVIGATION_TO_USER_LIST_SCREEN } from './routes';
+import Icon from 'react-native-vector-icons/MaterialIcons';  // Import the icon library
 
 const Drawer = createDrawerNavigator();
 
@@ -11,15 +12,25 @@ const RootDrawerNavigator = () => {
             <Drawer.Screen
                 name={NAVIGATION_TO_USER_LIST_SCREEN}
                 component={UserScreen}
-                options={{ drawerLabel: 'Users' }}
+                options={{
+                    drawerLabel: 'Users',
+                    drawerIcon: ({ color, size }) => (
+                        <Icon name="person" size={size} color={color} />  // User icon
+                    ),
+                }}
             />
             <Drawer.Screen
                 name={NAVIGATION_TO_HOME_SCREEN}
                 component={HomeScreen}
-                options={{ drawerLabel: 'Home' }}
+                options={{
+                    drawerLabel: 'Home',
+                    drawerIcon: ({ color, size }) => (
+                        <Icon name="home" size={size} color={color} />  // Home icon
+                    ),
+                }}
             />
         </Drawer.Navigator>
     );
 }
 
-export default RootDrawerNavigator
+export default RootDrawerNavigator;
